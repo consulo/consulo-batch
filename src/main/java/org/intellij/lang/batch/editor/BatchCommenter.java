2aa1123d893414f28cdc2f63ab9ab2bb94d4c059
VISTALL <vistall.valeriy@gmail.com>
2022-06-27 06:16:12 +0300

    drop old xml extensions

--- src/main/java/org/intellij/lang/batch/editor/BatchCommenter.java
+++ src/main/java/org/intellij/lang/batch/editor/BatchCommenter.java
@@ -1,18 +1,22 @@
 package org.intellij.lang.batch.editor;
 
+import consulo.annotation.component.ExtensionImpl;
 import consulo.language.CodeDocumentationAwareCommenter;
+import consulo.language.Language;
 import consulo.language.ast.IElementType;
 import consulo.language.psi.PsiComment;
+import org.intellij.lang.batch.BatchLanguage;
 import org.intellij.lang.batch.BatchTokenTypes;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 /**
  * @author wibotwi
  */
+@ExtensionImpl
 public class BatchCommenter implements CodeDocumentationAwareCommenter
 {
-
 	@Override
 	public IElementType getLineCommentTokenType()
 	{
@@ -85,5 +89,12 @@ public class BatchCommenter implements CodeDocumentationAwareCommenter
 	public String getCommentedBlockCommentSuffix()
 	{
 		return null;
+	}
+
+	@Nonnull
+	@Override
+	public Language getLanguage()
+	{
+		return BatchLanguage.INSTANCE;
 	}
 }
