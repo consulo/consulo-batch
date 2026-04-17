09ac0533793d06589dd232aed9a67f2f222b2a6f
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 16:49:14 +0300

    remove ide.api usage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,5 +8,26 @@ module consulo.batch
 	requires java.desktop;
 	requires forms.rt;
 
-	requires consulo.ide.api;
+	requires consulo.application.api;
+	requires consulo.code.editor.api;
+	requires consulo.color.scheme.api;
+	requires consulo.compiler.api;
+	requires consulo.component.api;
+	requires consulo.configurable.api;
+	requires consulo.execution.api;
+	requires consulo.execution.impl;
+	requires consulo.language.api;
+	requires consulo.language.impl;
+	requires consulo.language.editor.api;
+	requires consulo.localize.api;
+	requires consulo.logging.api;
+	requires consulo.module.api;
+	requires consulo.process.api;
+	requires consulo.project.api;
+	requires consulo.ui.api;
+	requires consulo.ui.ex.awt.api;
+	requires consulo.virtual.file.system.api;
+	requires consulo.util.io;
+	requires consulo.util.lang;
+	requires consulo.util.xml.serializer;
 }
